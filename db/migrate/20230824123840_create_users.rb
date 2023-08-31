dc138456f243792f523642d4a2c249cf53f4d19f
sachin <sachinp@shriffle.com>
2023-08-31 19:07:01 +0530

    create a user migration

--- db/migrate/20230824123840_create_users.rb
+++ db/migrate/20230824123840_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.integer :mobile_number
-      t.string :add
+      t.integer :age
       t.references :course , null: false, foreign_key: true
 
       t.timestamps
